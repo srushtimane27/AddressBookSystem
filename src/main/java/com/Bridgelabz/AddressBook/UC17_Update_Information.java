7d7e4e288ce967fcbc14d0b21861bb53de07e5b3
srushtimane27 <srushtimane31@gmail.com>
2022-09-18 00:06:32 +0530

    Update information in database

---
+++ src/main/java/com/Bridgelabz/AddressBook/UC17_Update_Information.java
@@ -0,0 +1,58 @@
+package com.Bridgelabz.AddressBook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UC17_Update_Information {
+    public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement stm = null;
+        String query = "update address_book.address_book set zip = '123456' where id = 14";
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("driver register");
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Srushti@2712");
+
+            System.out.println("connection establish");
+
+            stm = con.prepareStatement(query);
+            //   System.out.println("platform created");
+
+
+            stm.executeUpdate();
+
+            System.out.println("Data inserted");
+
+            //     System.out.println("Data inserted");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            if (con != null){
+                try{
+                    con.close();
+                }catch(SQLException s)
+                {
+                    System.out.println(s.getMessage());
+                }
+            }if (stm != null){
+                try {
+                    stm.close();
+                }catch (SQLException s){
+                    System.out.println(s.getMessage());
+
+                }if (sc != null){
+                    sc.close();
+                }
+            }
+        }
+    }
+}
+
+
